7ca3ebd5defbd9ba3d9848ab4b998ef1612268d9
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-05-23 09:15:01 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./components/HomePage/HomePage"
+import { NavLink, Route, Routes } from "react-router-dom";
+import HomePage from "./components/MoviesCast/HomePage"
+import MoviesPage from "./components/MoviesList/MoviesPage"
+
+return (
+    <div>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/movies">MoviesPage</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/movies/:moviedId">MovieDetailsPage</NavLink>
+                </li>
+            </ul>
+        </nav>
+         <Routes>
+            <Route path="/" element={<div>Home Page</div>}></Route>
+            <Route></Route>
+         </Routes>
+    </div>
+)
